7031f885dd7b1ae6d99c0287552dbab67aee591f
dafauzann <118601262+dafauzann@users.noreply.github.com>
2023-06-11 23:34:18 +0700

    Add files via upload

---
+++ airlangga_rent/template/privacy.php
@@ -0,0 +1,152 @@
+<?php include '../template/header.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rental Mobil Airlangga | Term of Use</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap">
+  
+<style>
+.body{
+    margin: 0;
+    padding: 0;
+}
+
+.page-header {
+    background-image: url("TG.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    border: 0 none;
+    margin: 0 auto;
+    padding: 200px;
+    position: relative;
+  }
+  .page-header_wrap {
+    padding:60px 0;
+    position:relative;	
+    text-align:center;
+  }
+  .page-heading {
+    z-index:1;
+    position:relative;
+  }
+  .page-heading h2, .page-heading h1 {
+    font-size:40px;
+    color: #ffffff;
+    margin: 0 auto;
+  }
+
+  .about-us {
+  padding: 60px 0;
+  background-color: #f8f8f8;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: left;
+}
+
+.about-us .row {
+  justify-content: center;
+}
+
+.about-us h1 {
+  font-size: 30px;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 20px;
+  text-align: center;
+}
+
+.about-us p {
+  font-size: 16px;
+  line-height: 1.8;
+  color: #333;
+  text-align: justify;
+}
+
+.about-us h5 {
+  font-size: 20px;
+  line-height: 1.8;
+  color: #333;
+  text-align: center;
+}
+
+</style>
+
+</head>
+<body>
+<section class="page-header contactus_page">
+  <div class="container">
+    <div class="page-header_wrap">
+      <div class="page-heading">
+        <h1>Syarat dan Ketentuan</h1>
+      </div>
+      <ul class="coustom-breadcrumb">
+        <li><a href="../home/home.php">Home</a></li>
+      </ul>
+    </div>
+  </div>
+  <!-- Dark Overlay-->
+  <div class="dark-overlay"></div>
+</section>
+
+<section class="about-us">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <h1>
+            Syarat Penggunaan Aplikasi Rental Mobil Airlangga
+        </h1>
+        <p>
+        Selamat datang di Aplikasi Rental Mobil. Privasi Anda sangat penting bagi kami,
+        dan kami berkomitmen untuk melindungi informasi pribadi yang Anda berikan saat menggunakan layanan kami.
+        Kebijakan privasi ini menjelaskan bagaimana kami mengumpulkan, menggunakan, dan melindungi informasi pribadi Anda.
+        Dengan menggunakan aplikasi ini, Anda menyetujui kebijakan privasi ini.
+        </p>
+        <br><br>
+        <h4> 1. Informasi yang Kami Kumpulkan </h4>
+         <p>Kami dapat mengumpulkan informasi pribadi Anda seperti nama, alamat email, nomor telepon, dan alamat saat Anda mendaftar atau menggunakan aplikasi kami.
+            Selain itu, kami juga dapat mengumpulkan informasi non-pribadi seperti jenis perangkat yang Anda gunakan, lokasi geografis, dan data penggunaan aplikasi.
+        </p>
+<br>
+           
+        <h4>2. Penggunaan Informasi</h4>
+        <p>Informasi pribadi yang kami kumpulkan digunakan untuk menyediakan layanan yang Anda minta, seperti proses reservasi dan pemesanan mobil.
+        Kami juga dapat menggunakan informasi tersebut untuk berkomunikasi dengan Anda mengenai pembaruan, penawaran khusus, atau layanan terkait yang mungkin menarik bagi Anda.
+        </p>
+<br>
+        <h4> 3. Penyimpanan dan Keamanan Informasi </h4>
+        <p>Kami mengambil langkah-langkah yang wajar untuk melindungi informasi pribadi Anda dari akses yang tidak sah, penggunaan yang salah, atau pengungkapan yang tidak sah.
+        Kami menyimpan informasi Anda pada sistem yang aman dan hanya menyimpannya selama diperlukan untuk tujuan yang ditentukan dalam kebijakan ini.
+        </p>
+<br>
+        <h4> 4. Pembagian Informasi dengan Pihak Ketiga </h4>
+        <p>Kami tidak akan menjual, menyewakan, atau menukar informasi pribadi Anda kepada pihak ketiga tanpa izin Anda, kecuali jika diwajibkan oleh hukum.
+        Namun, kami dapat membagikan informasi pribadi Anda kepada pihak ketiga yang bekerja sama dengan kami untuk menyediakan layanan yang Anda minta, seperti penyedia mobil sewaan.
+        </p>
+<br>
+        <h4> 5. Perubahan Kebijakan Privasi </h4>
+        <p>Kami dapat memperbarui kebijakan privasi ini dari waktu ke waktu. Setiap perubahan akan diumumkan melalui aplikasi atau melalui kontak yang Anda berikan kepada kami.
+        Pastikan untuk memeriksa kebijakan privasi ini secara berkala.
+        </p>
+<br>
+        <p>
+        Kami sangat menghargai privasi Anda dan berusaha untuk menjaga kepercayaan Anda dengan memberikan perlindungan yang tepat terhadap informasi pribadi Anda. 
+        Jika Anda memiliki pertanyaan atau kekhawatiran tentang kebijakan privasi kami, jangan ragu untuk menghubungi kami melalui informasi kontak yang tersedia di aplikasi ini.
+        </p>
+<br>
+        <h5>
+        Kami berterima kasih telah menggunakan Aplikasi Rental Mobil.
+        </h5>
+
+      </div>
+    </div>
+  </div>
+</section>
+<?php include '../template/footer.php'; ?>
