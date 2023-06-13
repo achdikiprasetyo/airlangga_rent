6bab1572abe93e2cbd50b275cb0358cfaae757a3
dafauzann <118601262+dafauzann@users.noreply.github.com>
2023-06-13 14:09:12 +0700

    Update admin_add_mobil.php

--- airlangga_rent/admin/admin_add_mobil.php
+++ airlangga_rent/admin/admin_add_mobil.php
@@ -3,6 +3,62 @@
 <html>
 <head>
     <title>Input dan Output Mobil</title>
+    <style>
+    body {
+        font-family: Arial, sans-serif;
+        background-color: #f2f2f2;
+        margin: 0;
+        padding: 20px;
+    }
+
+    h1 {
+        color: #333;
+    }
+
+    form {
+        background-color: #fff;
+        padding: 20px;
+        border-radius: 5px;
+        width: auto;
+        margin: 0 auto;
+    }
+
+    label {
+        display: block;
+        margin-bottom: 10px;
+        font-weight: bold;
+    }
+
+    input[type="text"],
+    input[type="number"],
+    select {
+        width: 100%;
+        padding: 8px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+        margin-bottom: 10px;
+        font-size: 14px;
+    }
+
+    input[type="file"] {
+        margin-top: 10px;
+    }
+
+    input[type="submit"] {
+        background-color: #4CAF50;
+        color: #fff;
+        border: none;
+        padding: 10px 20px;
+        border-radius: 4px;
+        cursor: pointer;
+        font-size: 16px;
+    }
+
+    input[type="submit"]:hover {
+        background-color: #45a049;
+    }
+</style>
 </head>
 <body>
     <h1>Input Mobil Baru</h1>
